5c9476b4e088825e404ea9d7cff14b9d4e902d35
Cedric Kloss <2001fflp@gmail.com>
2022-03-24 16:57:52 +0100

    Add Tests for GridFormatter

--- OsuPlayer.Extensions/ValueConverters/GridFormatter.cs
+++ OsuPlayer.Extensions/ValueConverters/GridFormatter.cs
@@ -7,10 +7,14 @@ public class GridFormatter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
+        if (value is not double or int) return 0;
         var width = System.Convert.ToDouble(value);
+
+        if (parameter is not double or int) return 0;
         var targetWidth = System.Convert.ToDouble(parameter);
 
-        return (int)Math.Ceiling(width / targetWidth);
+        if (targetWidth <= 0) return 0;
+        return (int) Math.Ceiling(width / targetWidth);
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
---
+++ OsuPlayer.Tests/ValueConverterTests/GridFormatterTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Globalization;
+using Material.Icons;
+using NUnit.Framework;
+using OsuPlayer.Extensions.ValueConverters;
+
+namespace OsuPlayer.Tests.ValueConverterTests;
+
+public class GridFormatterTests
+{
+    private GridFormatter _gridFormatter;
+    private Type _expectedInput = typeof(double);
+    private Type _expectedParameter = typeof(double);
+    private Type _expectedOutput = typeof(int);
+
+    [SetUp]
+    public void Setup()
+    {
+        _gridFormatter = new GridFormatter();
+    }
+
+    [TestCase(10)]
+    [TestCase("test")]
+    [TestCase(true)]
+    public void TestWrongInputHandled(object input)
+    {
+        Assert.IsNotInstanceOf(_expectedInput, input.GetType());
+        Assert.DoesNotThrow(() =>
+            _gridFormatter.Convert(input, _expectedOutput, input, CultureInfo.InvariantCulture));
+    }
+
+    [Test]
+    public void TestNullInputHandled()
+    {
+        Assert.DoesNotThrow(
+            () => _gridFormatter.Convert(null, _expectedOutput, null, CultureInfo.InvariantCulture));
+    }
+
+    [TestCase(new object[] {100.0, 100.0, 1})]
+    [TestCase(new object[] {100.0, 99.0, 2})]
+    public void TestCorrectUsage(object[] input)
+    {
+        var width = input[0];
+        var param = input[1];
+        var expected = input[2];
+        Assert.Multiple(() =>
+        {
+            Assert.IsInstanceOf(_expectedInput, width);
+            Assert.IsInstanceOf(_expectedParameter, param);
+            Assert.IsInstanceOf(_expectedOutput, expected);
+        });
+        object output = null;
+        Assert.DoesNotThrow(() =>
+            output = _gridFormatter.Convert(width, _expectedOutput, param, CultureInfo.InvariantCulture));
+        Assert.IsInstanceOf(_expectedOutput, output);
+        Assert.AreEqual(expected, output);
+    }
+
+    [TestCase(new object[] {"test", 100})]
+    [TestCase(new object[] {true, "abc"})]
+    [TestCase(new object[] {100.0, 0.0})]
+    public void TestOutputOnIncorrectInput(object[] input)
+    {
+        var width = input[0];
+        var param = input[1];
+        object? output = null;
+        Assert.DoesNotThrow(() =>
+            output = _gridFormatter.Convert(width, _expectedOutput, param, CultureInfo.InvariantCulture));
+        Assert.IsInstanceOf(_expectedOutput, output);
+        Assert.AreEqual(output, 0);
+    }
+}
