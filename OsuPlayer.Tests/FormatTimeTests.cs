06f40da1c229cb6e34f3e63f2b50e5e25568e9c2
Cedric Kloss <2001fflp@gmail.com>
2022-03-24 10:49:51 +0100

    TimeFormatter Tests

---
+++ OsuPlayer.Tests/FormatTimeTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using OsuPlayer.Extensions;
+
+namespace OsuPlayer.Tests;
+
+public class FormatTimeTests
+{
+    [TestCase(0, 1, 30, "1:30")]
+    [TestCase(0, 10, 30, "10:30")]
+    [TestCase(1, 1, 30, "1:01:30")]
+    public void TestTimeFormat(object[] input)
+    {
+        var hours = (int)input[0];
+        var minutes = (int)input[1];
+        var seconds = (int)input[2];
+        var expectedString = input[3];
+        var actualString = new TimeSpan(hours, minutes, seconds).FormatTime();
+        Assert.AreEqual(expectedString, actualString);
+    }
+}
