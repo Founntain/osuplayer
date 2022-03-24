3653b84eef0e709ffe5bfc34a806bf416f032f1b
Founntain <7@founntain.dev>
2022-03-24 15:08:22 +0100

    basic window

---
+++ OsuPlayer/Windows/CreateProfileWindow.axaml.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using OsuPlayer.IO.Storage.Config;
+using OsuPlayer.ViewModels;
+
+namespace OsuPlayer.Windows;
+
+public partial class CreateProfileWindow : ReactiveWindow<CreateProfileWindowViewModel>
+{
+    public CreateProfileWindow()
+    {
+        InitializeComponent();
+        
+        using var config = new Config();
+        TransparencyLevelHint = config.Read().TransparencyLevelHint;
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
---
+++ OsuPlayer/Windows/CreateProfileWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using OsuPlayer.ViewModels;
+using ReactiveUI;
+
+namespace OsuPlayer.Windows;
+
+public class CreateProfileWindowViewModel : BaseWindowViewModel
+{
+    private string _username;
+    private string _password;
+    private bool _isTosChecked;
+
+    public string Username
+    {
+        get => _username;
+        set => this.RaiseAndSetIfChanged(ref _username, value);
+    }
+
+    public string Password
+    {
+        get => _password;
+        set => this.RaiseAndSetIfChanged(ref _password, value);
+    }
+
+    public bool IsTosChecked
+    {
+        get => _isTosChecked;
+        set => this.RaiseAndSetIfChanged(ref _isTosChecked, value);
+    }
+
+    public CreateProfileWindowViewModel()
+    {
+        
+    }
+    
+}
