2267a913f017f64d5963fae4512f7052941bbc73
Cedric Kloss <2001fflp@gmail.com>
2022-03-24 16:41:06 +0100

    Change Settings view to UniformGrid for dynamic columns

---
+++ OsuPlayer.Extensions/ValueConverters/GridFormatter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace OsuPlayer.Extensions.ValueConverters;
+
+public class GridFormatter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var width = System.Convert.ToDouble(value);
+        var targetWidth = System.Convert.ToDouble(parameter);
+
+        return (int)Math.Ceiling(width / targetWidth);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
