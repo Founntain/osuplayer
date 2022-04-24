d59a228b3ad0e3c468eeef21f4d25b124d71cf05
Founntain <7@founntain.dev>
2022-04-24 11:45:34 +0200

    refactor and typo fixes

--- OsuPlayer.Extensions/UnhandledExceptionHandler.cs
+++ OsuPlayer.Extensions/UnhandledExceptionHandler.cs
@@ -21,15 +21,18 @@ public static class UnhandledExceptionHandler
     {
         CreatLogsDirectoryIfItsMissing();
 
-        var dateString = DateTime.UtcNow.ToString("yyyyMMdd_Hmmss");
+        var dateString = DateTime.UtcNow.ToString("yyyyMMdd_HHmmss");
 
-        File.WriteAllText($"logs/{dateString}.txt", "A shit, it looks like you did something that the osu!player did not like. If you think this is a bug, please report this crashlog to Founntain via the following methods:"
-                                                    + Environment.NewLine
-                                                    + Environment.NewLine + "GitHub: https://github.com/Founntain/osuplayer/issues/new?assignees=&labels=bug&template=bug_report.md&title=%5BBUG%5D+"
-                                                    + Environment.NewLine + "Discord: https://discord.gg/RJQSc5B"
-                                                    + Environment.NewLine + "Email: 7@founntain.dev"
-                                                    + Environment.NewLine
-                                                    + Environment.NewLine
-                                                    + ex.Message + Environment.NewLine + ex.StackTrace);
+        //Not using concatenation so it is better readable
+        var crashlog = "A shit, it looks like you did something that the osu!player did not like. If you think this is a bug, please report this crashlog to Founntain via the following methods:"
+                       + Environment.NewLine
+                       + Environment.NewLine + "GitHub: https://github.com/Founntain/osuplayer/issues/new?assignees=&labels=bug&template=bug_report.md&title=%5BBUG%5D+"
+                       + Environment.NewLine + "Discord: https://discord.gg/RJQSc5B"
+                       + Environment.NewLine + "Email: 7@founntain.dev"
+                       + Environment.NewLine
+                       + Environment.NewLine
+                       + ex.Message + Environment.NewLine + ex.StackTrace;
+        
+        File.WriteAllText($"logs/{dateString}.txt", crashlog);
     }
 }
