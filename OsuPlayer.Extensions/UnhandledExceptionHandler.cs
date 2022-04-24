0dd7952b171025b013267b08f38d56b2341a9222
Founntain <7@founntain.dev>
2022-04-24 11:54:49 +0200

    refactor

--- OsuPlayer.Extensions/UnhandledExceptionHandler.cs
+++ OsuPlayer.Extensions/UnhandledExceptionHandler.cs
@@ -26,10 +26,12 @@ public static class UnhandledExceptionHandler
         //Not using concatenation so it is better readable
         var crashlog = "A shit, it looks like you did something that the osu!player did not like. If you think this is a bug, please report this crashlog to Founntain via the following methods:"
                        + Environment.NewLine
-                       + Environment.NewLine + "GitHub: https://github.com/Founntain/osuplayer/issues/new?assignees=&labels=bug&template=bug_report.md&title=%5BBUG%5D+"
-                       + Environment.NewLine + "Discord: https://discord.gg/RJQSc5B"
-                       + Environment.NewLine + "Email: 7@founntain.dev"
+                       + Environment.NewLine + "🐈 GitHub: https://github.com/Founntain/osuplayer/issues/new?assignees=&labels=bug&template=bug_report.md&title=%5BBUG%5D+"
+                       + Environment.NewLine + "🗨️ Discord: https://discord.gg/RJQSc5B"
+                       + Environment.NewLine + "✉️ Email: 7@founntain.dev"
                        + Environment.NewLine
+                       + Environment.NewLine
+                       + "🛑 Error stacktrace below:"
                        + Environment.NewLine
                        + ex.Message + Environment.NewLine + ex.StackTrace;
         
