df6c07f59377a71956f2df703f51af9dc0743935
Founntain <7@founntain.dev>
2022-06-23 20:32:31 +0200

    Fixed crash for importing collections!

--- OsuPlayer.IO/DbReader/OsuDbReader.cs
+++ OsuPlayer.IO/DbReader/OsuDbReader.cs
@@ -115,6 +115,8 @@ public class OsuDbReader : BinaryReader
 
             CalculateMapLength(reader, out var setId, out var hash);
 
+            if (hashes.Keys.Contains(hash)) continue;
+            
             hashes.Add(hash, setId);
         }
 
