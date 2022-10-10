ca45d913bba5729545a2bbd0398b224a470d7548
Carola Kloss <2001fflp@gmail.com>
2022-10-10 17:22:50 +0200

    Add ShuffleAlgorithm struct for easier usage

---
+++ OsuPlayer.Data/OsuPlayer/Classes/ShuffleAlgorithm.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OsuPlayer.Data.OsuPlayer.Classes;
+
+public readonly struct ShuffleAlgorithm
+{
+    public Type Type { get; }
+    private readonly string _formattedName;
+
+    public override string ToString()
+    {
+        return _formattedName;
+    }
+
+    public ShuffleAlgorithm(Type type)
+    {
+        Type = type;
+        _formattedName = Regex.Replace(type.Name, "([A-Z])", " $1", RegexOptions.Compiled).Trim();
+    }
+}
