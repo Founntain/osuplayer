d2c268571d7aba2ca067912cb9cf9676ce90180d
Wrac4242 <wrac4242@gmail.com>
2022-10-28 20:25:00 +0100

    Add new API User Endpoints

--- OsuPlayer.Network/API/ApiEndpoints/ApiUserEndpoint.cs
+++ OsuPlayer.Network/API/ApiEndpoints/ApiUserEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia.Media.Imaging;
 using OsuPlayer.Data.API.Enums;
 using OsuPlayer.Data.API.Models.Beatmap;
+using OsuPlayer.Data.API.Models.Statistic;
 using OsuPlayer.Data.API.Models.User;
 using OsuPlayer.Network.Online;
 
@@ -162,5 +163,33 @@ public partial class ApiAsync
         };
 
         await ApiRequestAsync<UserOnlineStatusModel>("users", "setUserOnlineStatus", data);
+    }
+
+    /// <summary>
+    /// Gets API statistics from /api/statistics/getApiStatisticsV2
+    /// </summary>
+    /// <returns>Statistics as an <see cref="ApiStatisticsV3Model" /></returns>
+    public static async Task<ApiStatisticsV3Model> GetApiStatistics()
+    {
+        // default is new value due to possible null
+        return await GetRequestAsync<ApiStatisticsV3Model>("statistics", "getApiStatisticsV2") ?? new ApiStatisticsV3Model();
+    }
+
+    /// <summary>
+    /// Gets beatmap count from /api/beatmaps/getBeatmapsCount
+    /// </summary>
+    /// <returns>Beatmap count as an integer</returns>
+    public static async Task<int> GetBeatmapsCount()
+    {
+        return await GetRequestAsync<int>("beatmaps", "getBeatmapsCount");
+    }
+    
+    /// <summary>
+    /// Gets the amount of storage used on the database
+    /// </summary>
+    /// <returns>Disk space used (in MB) as a float</returns>
+    public static async Task<float> GetStorageAmount()
+    {
+        return await GetRequestAsync<float>("osuplayer", "getStorageAmount");
     }
 }
