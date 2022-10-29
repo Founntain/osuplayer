e187e721b2affd90b9b8a7d25f7b5edffa11feea
Wrac4242 <wrac4242@gmail.com>
2022-10-29 18:50:00 +0100

    Add code to update the fields on loading

--- OsuPlayer/Views/StatisticsViewModel.cs
+++ OsuPlayer/Views/StatisticsViewModel.cs
@@ -1,6 +1,8 @@
 using System.Reactive.Disposables;
 using OsuPlayer.Base.ViewModels;
+using OsuPlayer.Data.API.Models.Statistic;
 using ReactiveUI;
+using OsuPlayer.Network.API.ApiEndpoints;
 
 namespace OsuPlayer.Views;
 
@@ -13,27 +15,66 @@ public class StatisticsViewModel : BaseViewModel
         this.WhenActivated(Block);
     }
 
-    private int _users = 1234;
-    private int _translators = 1234;
-    private int _songsPlayed = 1234;
-    private int _xpEarned = 1234;
-    private int _communityLevel = 1234;
-    private int _xpLeft = 1234;
+    private uint _users = 1234;
+    private uint _translators = 1234;
+    private ulong _songsPlayed = 1234;
+    private ulong _xpEarned = 1234;
+    private uint _communityLevel = 1234;
+    private ulong _xpLeft = 1234;
     private int _beatmapsTracked = 1234;
-    private int _mbUsed = 1234;
-    private int _playerAge = 1234;
-    public int Users { get => _users; set => this.RaiseAndSetIfChanged(ref _users, value);}
-    public int Translators { get => _translators; set => this.RaiseAndSetIfChanged(ref _translators, value); }
-    public int SongsPlayed { get => _songsPlayed; set => this.RaiseAndSetIfChanged(ref _songsPlayed, value);}
-    public int XpEarned { get => _xpEarned; set => this.RaiseAndSetIfChanged(ref _xpEarned, value);}
-    public int CommunityLevel { get => _communityLevel; set => this.RaiseAndSetIfChanged(ref _communityLevel, value);}
-    public int XpLeft { get => _xpLeft; set => this.RaiseAndSetIfChanged(ref _xpLeft, value);}
+    private float _mbUsed = 1234f;
+    private string _playerAge = "1234";
+    public uint Users { get => _users; set => this.RaiseAndSetIfChanged(ref _users, value);}
+    public uint Translators { get => _translators; set => this.RaiseAndSetIfChanged(ref _translators, value); }
+    public ulong SongsPlayed { get => _songsPlayed; set => this.RaiseAndSetIfChanged(ref _songsPlayed, value);}
+    public ulong XpEarned { get => _xpEarned; set => this.RaiseAndSetIfChanged(ref _xpEarned, value);}
+    public uint CommunityLevel { get => _communityLevel; set => this.RaiseAndSetIfChanged(ref _communityLevel, value);}
+    public ulong XpLeft { get => _xpLeft; set => this.RaiseAndSetIfChanged(ref _xpLeft, value);}
     public int BeatmapsTracked { get => _beatmapsTracked; set => this.RaiseAndSetIfChanged(ref _beatmapsTracked, value);}
-    public int MbUsed { get => _mbUsed; set => this.RaiseAndSetIfChanged(ref _mbUsed, value);}
-    public int PlayerAge { get => _playerAge; set => this.RaiseAndSetIfChanged(ref _playerAge, value);}
+    public float MbUsed { get => _mbUsed; set => this.RaiseAndSetIfChanged(ref _mbUsed, value);}
+    public string PlayerAge { get => _playerAge; set => this.RaiseAndSetIfChanged(ref _playerAge, value);}
 
     private void Block(CompositeDisposable disposable)
     {
-        
+        // done separately so they can fail independently
+        UpdateApiStatistics();
+        UpdateBeatmapCount();
+        UpdateStorageAmount();
+        UpdateDate();
     }
+
+    private void UpdateDate()
+    {
+        TimeSpan timeSince = DateTime.Now.Subtract(new DateTime(2017, 11, 1));
+        
+        DateTime time = DateTime.MinValue + timeSince;
+
+        // note: MinValue is 1/1/1 so we have to subtract...
+        int years = time.Year - 1;
+        int months = time.Month - 1;
+        int days = time.Day - 1;
+        
+        PlayerAge = $"{years} years, {months} months, {days} days, {timeSince.Hours}:{timeSince.Minutes}:{timeSince.Seconds} old";
+    }
+
+    private async void UpdateApiStatistics()
+    {
+        ApiStatisticsV3Model statistics = await ApiAsync.GetApiStatistics();
+        Users = statistics.TotalUserCount;
+        Translators = statistics.TranslatorCount;
+        XpEarned = statistics.CommunityTotalXp;
+        CommunityLevel = statistics.CommunityLevel;
+        XpLeft = statistics.CommunityXpLeft;
+    }
+
+    private async void UpdateStorageAmount()
+    {
+        MbUsed = await ApiAsync.GetStorageAmount();
+    }
+
+    private async void UpdateBeatmapCount()
+    {
+        BeatmapsTracked = await ApiAsync.GetBeatmapsCount();
+    }
+
 }
