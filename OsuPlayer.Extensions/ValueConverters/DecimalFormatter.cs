cb23b7094ae1f629e303ab069a4fafff1238258a
Carola Kloss <2001fflp@gmail.com>
2022-12-02 10:17:22 +0100

    Add number formatter for stat view

---
+++ OsuPlayer.Extensions/ValueConverters/DecimalFormatter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace OsuPlayer.Extensions.ValueConverters;
+
+public class DecimalFormatter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (targetType != typeof(string)) return null;
+
+        if (value is not IFormattable number) return "";
+
+        var format = number.ToString("N0", CultureInfo.CurrentCulture);
+
+        return format;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
