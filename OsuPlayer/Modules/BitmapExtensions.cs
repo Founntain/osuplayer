fd160a5489bf13ded9c4c52cf1c72b7dff7f5b00
Founntain <7@founntain.dev>
2023-06-02 12:04:14 +0200

    removed old code

--- OsuPlayer/Modules/BitmapExtensions.cs
+++ OsuPlayer/Modules/BitmapExtensions.cs
@@ -5,57 +5,7 @@ namespace OsuPlayer.Modules;
 
 public static class BitmapExtensions
 {
-    public static Bitmap BlurBitmap(this Bitmap bitmap, float blurRadius = 10f, float opacity = 1f)
-    {
-        SKBitmap originalBitmap = null;
-        var blurSigma = blurRadius / 2;
-
-        using (var stream = new MemoryStream())
-        {
-            bitmap.Save(stream);
-
-            stream.Position = 0;
-            
-            originalBitmap = SKBitmap.Decode(stream);
-        }
-
-        var originalImage = SKImage.FromBitmap(originalBitmap);
-        
-        var blurFilter = SKImageFilter.CreateBlur(blurRadius, blurSigma);
-        
-        // Create the paint with the blur filter
-        var paint = new SKPaint();
-        paint.ImageFilter = blurFilter;
-        
-        paint.Color = new SKColor(alpha: (byte)(255 * opacity), red: 0, green: 0, blue: 0); // Set the opacity using the Color property
-        
-        // Create the bitmap for the blurred image
-        var blurredBitmap = new SKBitmap(originalBitmap.Width, originalBitmap.Height);
-        
-        // Draw the original image with the blur effect onto the blurred bitmap
-        using (var canvas = new SKCanvas(blurredBitmap))
-        {
-            // canvas.Clear(SKColors.Black);
-            
-            canvas.DrawImage(originalImage, 0, 0, paint);
-        }
-
-        Bitmap blurredAvaloniaBitmap = null;
-        
-        // Convert the SkiaSharp SKBitmap back to an Avalonia Bitmap
-        using (var stream = new MemoryStream())
-        {
-            blurredBitmap.Encode(stream, SKEncodedImageFormat.Png, 80);
-
-            stream.Position = 0;
-            
-            blurredAvaloniaBitmap = new Bitmap(stream);
-        }
-
-        return blurredAvaloniaBitmap;
-    }
-    
-    public static Bitmap BlurBitmap(string imagePath, float blurRadius = 10f, float opacity = 1f)
+    public static Bitmap BlurBitmap(string imagePath, float blurRadius = 10f, float opacity = 1f, int quality = 80)
     {
         using var stream = File.OpenRead(imagePath);
         using var skBitmap = SKBitmap.Decode(stream);
@@ -78,7 +28,7 @@ public static class BitmapExtensions
         using var image = surface.Snapshot();
         using var outputStream = new MemoryStream();
 
-        image.Encode(SKEncodedImageFormat.Png, 80).SaveTo(outputStream);
+        image.Encode(SKEncodedImageFormat.Png, quality).SaveTo(outputStream);
         outputStream.Position = 0;
 
         return new Bitmap(outputStream);
