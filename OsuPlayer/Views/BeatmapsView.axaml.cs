28da820d61eebcd985d6eb561e1bf01358c6af5b
Founntain <7@founntain.dev>
2023-09-01 17:43:56 +0200

    BeatmapView implementation

--- OsuPlayer/Views/BeatmapsView.axaml.cs
+++ OsuPlayer/Views/BeatmapsView.axaml.cs
@@ -1,7 +1,17 @@
-﻿using Avalonia;
+﻿using System.Threading.Tasks;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Nein.Base;
+using Nein.Extensions;
+using OsuPlayer.Api.Data.API.RequestModels.Beatmap;
+using OsuPlayer.Api.Data.API.ResponseModels;
+using OsuPlayer.Extensions;
+using OsuPlayer.Network.API.Service.NorthFox;
+using OsuPlayer.Network.API.Service.NorthFox.Endpoints;
+using Splat;
 
 namespace OsuPlayer.Views;
 
@@ -15,5 +25,57 @@ public partial class BeatmapsView : ReactiveControl<BeatmapsViewModel>
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+
+    private async void SearchBeatmaps_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (ViewModel == null) return;
+
+        await ViewModel.SearchBeatmaps();
+    }
+
+    private void ToggleFilterMenu_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (ViewModel == null) return;
+        
+        ViewModel.IsFilterMenuOpen = !ViewModel.IsFilterMenuOpen;
+    }
+
+    private async void PreviousPage_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if(ViewModel == null) return;
+
+        var newPage = ViewModel.CurrentPage - 1;
+
+        if (newPage <= 0)
+        {
+            newPage = ViewModel.TotalPages;
+        }
+
+        await ViewModel.SearchBeatmaps(newPage);
+    }
+
+    private async void NextPage_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if(ViewModel == null) return;
+
+        var newPage = ViewModel.CurrentPage + 1;
+
+        if (newPage > ViewModel.TotalPages)
+        {
+            newPage = 1;
+        }
+
+        await ViewModel.SearchBeatmaps(newPage);
+    }
+
+    private async void CurrentPage_OnKeyUp(object? sender, KeyEventArgs e)
+    {
+        if(e.Key != Key.Return || e.Key == Key.Enter || ViewModel == null) return;
+        
+        if(ViewModel.CurrentPage > ViewModel.TotalPages)
+            ViewModel.CurrentPage = ViewModel.TotalPages;
+
+        await ViewModel.SearchBeatmaps(ViewModel.CurrentPage);
     }
 }
--- OsuPlayer/Views/BeatmapsViewModel.cs
+++ OsuPlayer/Views/BeatmapsViewModel.cs
@@ -1,8 +1,12 @@
 ﻿using System.Reactive.Disposables;
+using System.Threading.Tasks;
 using Nein.Base;
 using Nein.Extensions;
-using OsuPlayer.Network.API.ModelExtensions;
-using OsuPlayer.Network.API.Service.Endpoints;
+using OsuPlayer.Api.Data.API.EntityModels;
+using OsuPlayer.Api.Data.API.Enums;
+using OsuPlayer.Api.Data.API.RequestModels.Beatmap;
+using OsuPlayer.Api.Data.API.ResponseModels;
+using OsuPlayer.Network.API.Service.NorthFox.Endpoints;
 using ReactiveUI;
 using Splat;
 
@@ -10,9 +14,98 @@ namespace OsuPlayer.Views;
 
 public class BeatmapsViewModel : BaseViewModel
 {
-    private ObservableCollection<ApiBeatmapModel> _beatmaps = new ();
+    private ObservableCollection<BeatmapModel> _beatmaps = new();
 
-    public ObservableCollection<ApiBeatmapModel> Beatmaps
+    private int _currentPage;
+    private int _totalPages;
+    private string _searchArtist;
+    private string _searchTitle;
+    private string _searchBeatmapSetId;
+    private string _searchBeatmapId;
+    private FilterCondition _searchArtistFilterCondition;
+    private FilterCondition _searchTitleFilterCondition;
+    private FilterCondition _searchBeatmapSetIdFilterCondition;
+    private FilterCondition _searchBeatmapIdFilterCondition;
+    private bool _searchingBeatmaps;
+    private bool _isFilterMenuOpen;
+
+    public bool SearchingBeatmaps
+    {
+        get => _searchingBeatmaps;
+        set => this.RaiseAndSetIfChanged(ref _searchingBeatmaps, value);
+    }
+
+    public int TotalPages
+    {
+        get => _totalPages;
+        set => this.RaiseAndSetIfChanged(ref _totalPages, value);
+    }
+
+    public int CurrentPage
+    {
+        get => _currentPage;
+        set => this.RaiseAndSetIfChanged(ref _currentPage, value);
+    }
+
+    public bool IsFilterMenuOpen
+    {
+        get => _isFilterMenuOpen;
+        set => this.RaiseAndSetIfChanged(ref _isFilterMenuOpen, value);
+    }
+
+    public string SearchBeatmapSetId
+    {
+        get => _searchBeatmapSetId;
+        set => this.RaiseAndSetIfChanged(ref _searchBeatmapSetId, value);
+    }
+
+    public string SearchBeatmapId
+    {
+        get => _searchBeatmapId;
+        set => this.RaiseAndSetIfChanged(ref _searchBeatmapId, value);
+    }
+
+    public string SearchTitle
+    {
+        get => _searchTitle;
+        set => this.RaiseAndSetIfChanged(ref _searchTitle, value);
+    }
+
+    public string SearchArtist
+    {
+        get => _searchArtist;
+        set => this.RaiseAndSetIfChanged(ref _searchArtist, value);
+    }
+
+    public FilterCondition SearchBeatmapSetIdFilterCondition
+    {
+        get => _searchBeatmapSetIdFilterCondition;
+        set => this.RaiseAndSetIfChanged(ref _searchBeatmapSetIdFilterCondition, value);
+    }
+
+    public FilterCondition SearchBeatmapIdFilterCondition
+    {
+        get => _searchBeatmapIdFilterCondition;
+        set => this.RaiseAndSetIfChanged(ref _searchBeatmapIdFilterCondition, value);
+    }
+
+    public FilterCondition SearchArtistFilterCondition
+    {
+        get => _searchArtistFilterCondition;
+        set => this.RaiseAndSetIfChanged(ref _searchArtistFilterCondition, value);
+    }
+
+    public FilterCondition SearchTitleFilterCondition
+    {
+        get => _searchTitleFilterCondition;
+        set => this.RaiseAndSetIfChanged(ref _searchTitleFilterCondition, value);
+    }
+
+    public IEnumerable<FilterCondition> StringFilterConditions => Enum.GetValues<FilterCondition>().Where(x => (int)x <= 2);
+
+    public IEnumerable<FilterCondition> IntFilterConditions => Enum.GetValues<FilterCondition>().Where(x => (int)x > 2);
+
+    public ObservableCollection<BeatmapModel> Beatmaps
     {
         get => _beatmaps;
         set => this.RaiseAndSetIfChanged(ref _beatmaps, value);
@@ -21,7 +114,7 @@ public class BeatmapsViewModel : BaseViewModel
     public BeatmapsViewModel()
     {
         Activator = new ViewModelActivator();
-        
+
         this.WhenActivated(Block);
     }
 
@@ -29,15 +122,45 @@ public class BeatmapsViewModel : BaseViewModel
     {
         Disposable.Create(() => { }).DisposeWith(disposables);
 
+        SearchBeatmapIdFilterCondition = FilterCondition.GreaterOrEqualThan;
+        SearchBeatmapSetIdFilterCondition = FilterCondition.GreaterOrEqualThan;
+
+        // If we have beatmaps already loaded and reload the view we don't want to load them again
+        if (Beatmaps?.Count > 0) return;
+
         var api = Locator.Current.GetService<NorthFox>();
 
-        var beatmaps = await api.GetBeatmapsPaged(pageSize: 100);
+        await SearchBeatmaps();
+    }
+    
+    public async Task<BeatmapSearchResponse> SearchBeatmaps(int newPage = 1, int pageSize = 64)
+    {
+        var api = Locator.Current.GetService<NorthFox>();
 
-        if (beatmaps == null || beatmaps.Count == 0)
-        {
-            return;
-        }
+        SearchingBeatmaps = true;
         
-        Beatmaps = beatmaps.OrderByDescending(x => x.TimesPlayed).ToObservableCollection();
+        var beatmaps = await api.Beatmap.GetBeatmapsPaged(new SearchBeatmapModel
+        {
+            Page = newPage,
+            PageSize = pageSize,
+            Artist = SearchArtist,
+            ArtistFilterCondition = SearchArtistFilterCondition,
+            Title = SearchTitle,
+            TitleFilterCondition = SearchTitleFilterCondition,
+            BeatmapSetId = SearchBeatmapSetId,
+            BeatmapSetIdFilterCondition = SearchBeatmapSetIdFilterCondition,
+            BeatmapId = SearchBeatmapId,
+            BeatmapIdFilterCondition = SearchBeatmapIdFilterCondition
+        });
+        
+        SearchingBeatmaps = false;
+
+        if (beatmaps == null || beatmaps.Beatmaps.Count == 0) return beatmaps;
+        
+        Beatmaps = beatmaps.Beatmaps.ToObservableCollection();
+        CurrentPage = beatmaps.CurrentPage;
+        TotalPages = beatmaps.TotalPages;
+
+        return beatmaps;
     }
 }
