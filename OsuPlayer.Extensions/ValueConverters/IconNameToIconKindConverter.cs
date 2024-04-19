a3fcf124939863763584a6bb00b465b9825f9d0f
Founntain <7@founntain.dev>
2024-04-19 21:53:22 +0200

    Update Avalonia packages and remove redundant resources
    The Avalonia packages were updated along with corresponding code adjustments. Unnecessary resources declaration and usage were removed from multiple project files and views. Also, the new IconNameToIconKind value converter was introduced, allowing the conversion of icon names to their respective kinds directly in XAML.

---
+++ OsuPlayer.Extensions/ValueConverters/IconNameToIconKindConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+using Material.Icons;
+
+namespace OsuPlayer.Extensions.ValueConverters;
+
+public class IconNameToIconKindConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not string s) return MaterialIconKind.Error;
+
+        try
+        {
+            return Enum.Parse<MaterialIconKind>(s);
+        }
+        catch (Exception ex)
+        {
+            return MaterialIconKind.Error;
+        }
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
